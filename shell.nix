392868e7dc14cbfc8760a080f4dbb8d2df09b494
ShadowCurse <shadowcurse@noreply.codeberg.org>
2026-03-01 19:27:54 +0000

    bindings: add sdl3 bindings, generator and shell.nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,14 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell {
+  LIBC_INCLUDE_PATH = "${pkgs.lib.makeIncludePath [pkgs.glibc]}";
+  SDL3_INCLUDE_PATH = "${pkgs.lib.makeIncludePath [pkgs.sdl3]}";
+  LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [pkgs.glibc pkgs.sdl3]}";
+
   buildInputs = with pkgs; [
+    clang
     pkg-config
     sdl3
+    libclang
   ];
 }
 
