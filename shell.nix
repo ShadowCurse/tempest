d34e24dd82e8aec29b782a94cfe84d1e5374f8f6
ShadowCurse <shadowcurse@noreply.codeberg.org>
2026-03-02 23:11:21 +0000

    build: nix: add vulkan loader

--- shell.nix
+++ shell.nix
@@ -2,7 +2,8 @@
 pkgs.mkShell {
   LIBC_INCLUDE_PATH = "${pkgs.lib.makeIncludePath [pkgs.glibc]}";
   SDL3_INCLUDE_PATH = "${pkgs.lib.makeIncludePath [pkgs.sdl3]}";
-  LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [pkgs.glibc pkgs.sdl3 pkgs.libclang]}";
+  VULKAN_INCLUDE_PATH = "${pkgs.lib.makeIncludePath [pkgs.vulkan-headers]}";
+  LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [pkgs.glibc pkgs.sdl3 pkgs.libclang pkgs.vulkan-loader]}";
 
   buildInputs = with pkgs; [
     clang
