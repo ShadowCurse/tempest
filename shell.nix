5fa6c67cdb8f294f989c36748221c9a3309a1de0
ShadowCurse <shadowcurse@noreply.codeberg.org>
2026-03-02 20:32:27 +0000

    build: nix: add libclang for the bindings gen

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
 pkgs.mkShell {
   LIBC_INCLUDE_PATH = "${pkgs.lib.makeIncludePath [pkgs.glibc]}";
   SDL3_INCLUDE_PATH = "${pkgs.lib.makeIncludePath [pkgs.sdl3]}";
-  LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [pkgs.glibc pkgs.sdl3]}";
+  LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [pkgs.glibc pkgs.sdl3 pkgs.libclang]}";
 
   buildInputs = with pkgs; [
     clang
