54e50abc32102ae5fbb745b59a6acf06fbc33810
ShadowCurse <shadowcurse@noreply.codeberg.org>
2026-03-08 15:07:38 +0000

    build: add command to rebuild shaders

--- shell.nix
+++ shell.nix
@@ -4,12 +4,14 @@ pkgs.mkShell {
   SDL3_INCLUDE_PATH = "${pkgs.lib.makeIncludePath [pkgs.sdl3]}";
   VULKAN_INCLUDE_PATH = "${pkgs.lib.makeIncludePath [pkgs.vulkan-headers]}";
   LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [pkgs.glibc pkgs.sdl3 pkgs.libclang pkgs.vulkan-loader]}";
+  VK_LAYER_PATH = "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
 
   buildInputs = with pkgs; [
-    clang
+    renderdoc
+    spirv-cross
+    shaderc
     pkg-config
     sdl3
     libclang
   ];
 }
-
